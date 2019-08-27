590e2af04f0af1fa669c2bff904a40ad4b541c54
blimpus <a.holly96@gmail.com>
2019-08-27 14:40:19 -0500

    commit before starting named queries

--- src/main/java/com/pluralsight/repository/GoalRepository.java
+++ src/main/java/com/pluralsight/repository/GoalRepository.java
@@ -1,13 +1,20 @@
 package com.pluralsight.repository;
 
+import java.util.List;
+
 import org.springframework.transaction.annotation.Transactional;
 
 import com.pluralsight.model.Goal;
+import com.pluralsight.model.GoalReport;
 
 @Transactional
 public interface GoalRepository {
 	
 	@Transactional
 	Goal save(Goal goal);
+
+	List<Goal> loadAll();
+
+	List<GoalReport> findAllGoalReports();
 
 }
--- src/main/java/com/pluralsight/repository/GoalRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/GoalRepositoryImpl.java
@@ -1,11 +1,15 @@
 package com.pluralsight.repository;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Goal;
+import com.pluralsight.model.GoalReport;
 
 @Repository("goalRepository")
 public class GoalRepositoryImpl implements GoalRepository {
@@ -20,6 +24,26 @@ public class GoalRepositoryImpl implements GoalRepository {
 		em.flush();
 		
 		return goal;
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public List<Goal> loadAll() {
+		Query query = em.createQuery("Select g from Goal g");
+		
+		List goals = query.getResultList();
+		
+		
+		
+		return goals;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<GoalReport> findAllGoalReports() {
+		
+		Query query = em.createQuery("Select new com.pluralsight.model.GoalReport(g.minutes,e.minutes,e.activity) " + 
+				"from Goal g, Exercise e where g.id = e.goal.id");
+		
+		return query.getResultList();
 	}
 
 }
